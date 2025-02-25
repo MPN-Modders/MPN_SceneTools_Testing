478a592b36cf4325eb463b2683d56a26d2aef923
Michael Swain <swain@projectnexus2.com>
2025-02-25 12:25:15 -0500

    Small metadata update

--- Assets/Code/SceneObjs/Cover_Point.cs
+++ Assets/Code/SceneObjs/Cover_Point.cs
@@ -4,6 +4,6 @@ using System.Collections.Generic;
 
 public class Cover_Point : MonoBehaviour
 {
-    [Tooltip("When shooting from cover, this is where the shooter will snap to while firing.")]
+    [Tooltip("When shooting from cover, this is where the shooter will snap to while firing. Fully optional, and rarely used in M:PN.")]
     public Transform LeapOutPoint;
 }
--- Assets/Code/Widgets/Remover_WaveCheckpoint.cs
+++ Assets/Code/Widgets/Remover_WaveCheckpoint.cs
@@ -5,14 +5,19 @@ using System.Collections.Generic;
 public class Remover_WaveCheckpoint : MonoBehaviour
 {
     [Range(-1, 100)]
+    [Tooltip("Leave at -1 for no min value.")]
     public int Imprint_Min = -1;
     [Range(-1, 100)]
+    [Tooltip("Leave at -1 for no max value.")]
     public int Imprint_Max = -1;
 
     [Range(-1, 10)]
+    [Tooltip("Leave at -1 for no min value.")]
     public int Checkpoint_Min = -1;
     [Range(-1, 10)]
+    [Tooltip("Leave at -1 for no max value.")]
     public int Checkpoint_Max = -1;
     [Space(10)]
-    public bool RemoveIfTrue = false;       // Invert the check.
+    [Tooltip("If TRUE, we remove the attached object if the above conditions are met. Otherwise on FALSE, we leave the attached object alone.")]
+    public bool RemoveIfTrue = true;
 }
