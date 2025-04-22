6695b631c3f5e97b6aea37425657f785afcfb358
Michael Swain <swain@projectnexus2.com>
2025-04-22 13:53:45 -0400

    Assigning SerialNumbers to Items from Marker_Item

--- Assets/Code/SceneObjs/Marker_Item.cs
+++ Assets/Code/SceneObjs/Marker_Item.cs
@@ -6,7 +6,10 @@ public class Marker_Item : Marker
     public string MadCard;
     public bool PhysicsOn = false;
     public bool SpawnIntoParent = false;
+    [Tooltip("The SerialNumber to assign to your spawned weapon. NOTE: Marker_Item does not use its SerialNumber value (above) to assign *itself* a SN# like some other Marker spawners.")]
+    public string AssignSerialNumber;
+
+    [Space(10)]
     public int BonusMags = 0;
     public Vector3 SpawnForce = Vector3.zero;   // in World Space
-
 }
