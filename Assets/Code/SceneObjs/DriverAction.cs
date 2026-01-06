382464995ff73858c582dad5ddc3bb3e4f574e30
Michael Swain <thatolswain@gmail.com>
2026-01-06 16:48:34 -0500

    AdditiveForce added to DriverActions
    AdditiveForce lets forces be added to a velocity rather than directly applied (useful for multiple DriverActions affecting the same Rigidbody)

--- Assets/Code/SceneObjs/DriverAction.cs
+++ Assets/Code/SceneObjs/DriverAction.cs
@@ -31,15 +31,22 @@ public class DriverAction_DIVERSE : DriverAction_PHYSICS
 {
 	[Tooltip("Applies this DriverAction as physics forces if it has a Rigidbody that isn *not* set to Kinematic.")]
 	public bool ForcesAsPhysics = true;
+	[Tooltip("If TRUE, adds force to the Rigidbody rather than directly setting it. Useful for when multiple DriverActions will affect the same Rigidbody. Just don't forget the drag values on your Rigidbody or you're gonna have a bad time.")]
+	public bool AdditiveForce = false; 
 }
 public class DriverAction_AXIS : DriverAction_DIVERSE
 {
+	[Tooltip("The axis to rotate around (RotateAxis) or the direction to move (MoveAxis).")]
 	public Vector3 Axis = Vector3.forward;
+	[Tooltip("Axis will count as normalized no matter what.")]
 	public bool NormalizeAxis = false;
+	[Tooltip("Is the Axis relative to world space, local parent space, or the Vessel itself?")]
 	public AxisTypes UseAxis = AxisTypes.Global; // If TRUE, we use the axis of the parent instead of our own right/up/forward.
 }
 public class DriverAction_TARGETED : DriverAction_DIVERSE
 {
+	[Tooltip("The thing to move or rotate towards.")]
 	public TargetTypes TargetType = TargetTypes.Target;
+	[Tooltip("Only used when TargetType is set to VesselAxis. This is the direction relative to the Vessel's current rotation (so, a VesselAxis of (0,0,1) would effectively be the Vessel's current forward).")]
 	public Vector3 VesselAxis = Vector3.forward; // The local direction of our linked Vessel.
 }
